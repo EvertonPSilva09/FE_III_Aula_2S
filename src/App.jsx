5359a3c7c17d453f6080f2d710b3c4d43c53e79b
Everton Silva <everton.p.silva@outlook.com>
2023-02-15 18:51:25 -0300

    Exercício aula 02

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+//function App() {
+//  return <h1>Meu primeiro componente</h1>;
+//}
+
+const App = () => {
+  return (
+    <>
+    <h1>Olá mundo</h1>
+    <img src="./src/assets/AnySourJanenschia-size_restricted.gif"/>
+  </>)
+}
+
+export default App;
